a0d794113c5df9fc29e54984b2289fe75ec1ff7d
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-22 13:05:53 +0700

    add header component

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+	return (
+		<nav>
+			<a className="" href="/">
+				<span>Sell it</span>
+			</a>
+			<a className="" href="/">
+				<span>Add Product</span>
+			</a>
+		</nav>
+	);
+};
