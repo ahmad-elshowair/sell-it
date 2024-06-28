0572e472e98718e9aa289ae559d81db6e0ab50f5
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-28 09:22:38 +0700

    "Updated Footer component to include Link from next/link and modified styles and layout"

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const Footer = ({ font }: { font: string }) => {
 	const date = new Date();
 	return (
 		<footer
-			className={`py-4 px-12 bg-gradient-to-tr from-green-600 via-orange-200 to-green-400 ${font}`}>
-			<h3 className="font-bold text-xl text-orange-50  lowercase">
-				© A.E.DEV {date.getFullYear()}
+			className={`py-4 px-12 bg-gradient-to-tr from-green-600 via-orange-200 to-green-400 ${font} text-center`}>
+			<h3 className="font-bold text-4xl text-orange-50">
+				©{" "}
+				<Link
+					href={"https://www.linkedin.com/in/ahmad-elshowair/"}
+					className="hover:underline duration-500 transition-all ease-in-out">
+					A.E.DEV
+				</Link>{" "}
+				{date.getFullYear()}
 			</h3>
 		</footer>
 	);
