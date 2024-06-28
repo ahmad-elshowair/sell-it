e6676409d70180575f04b11efb8988b258b92822
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-28 09:25:43 +0700

    "Updated header component: changed padding to py-4, increased font size of logo and navigation links."

--- components/header.tsx
+++ components/header.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 const Header = ({ font }: { font?: string }) => {
 	return (
 		<header
-			className={`py-3 bg-gradient-to-tr from-green-600 via-orange-200 to-green-400 ${font}`}>
+			className={`py-4 bg-gradient-to-tr from-green-600 via-orange-200 to-green-400 ${font}`}>
 			<nav className="flex justify-between items-center px-12 mx-auto">
 				<Link
 					className=" hover:scale-105 hover:drop-shadow-xl duration-700 ease-out"
 					href="/">
-					<span className="lowercase  text-orange-50 font-bold text-2xl">
+					<span className="lowercase  text-orange-50 font-bold text-4xl">
 						<span className="text-4xl">🏪</span>Sell it
 					</span>
 				</Link>
 				<Link
-					className="text-xl hover:scale-105 duration-500 ease-in hover:drop-shadow-xl"
+					className="text-4xl hover:scale-105 duration-500 ease-in hover:drop-shadow-xl"
 					href="/sell">
 					<span className="lowercase text-orange-50 font-bold">
 						Add Product
